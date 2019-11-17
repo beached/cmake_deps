8f49543fede398d8fcca8abea5460af7aacba3e2
Darrell Wright <dwright-git@dawdevel.ca>
2019-11-17 03:14:20 -0500

    Set ci build to static

--- ci_build.cmd
+++ ci_build.cmd
@@ -1,5 +1,5 @@
 rename glean.cmake.renamewin glean.cmake
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="%BOOST_ROOT%" -DCMAKE_TOOL_CHAIN_FILE="%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DUSE_STATIC_BOOST=ON -DBOOST_ROOT="%BOOST_ROOT%" -DCMAKE_TOOL_CHAIN_FILE="%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake" ..
 cmake --build . --config Release --target -j 2
