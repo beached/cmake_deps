fce90d64f008e57683318bd0bad1d5914652c625
Darrell Wright <dwright-git@dawdevel.ca>
2019-11-17 01:22:27 -0500

    defining BOOST_ROOT in cmake commandline

--- ci_build.cmd
+++ ci_build.cmd
@@ -2,5 +2,5 @@
 md build
 cd build
 pwd
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOL_CHAIN_FILE="%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="%BOOST_ROOT%" -DCMAKE_TOOL_CHAIN_FILE="%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake" ..
 cmake --build . --config Release --target full -j 2
